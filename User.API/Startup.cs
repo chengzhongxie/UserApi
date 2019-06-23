a427174fa81cecbe06f73ccdbab0248900ef8f34
xiecz <chengzhongxie@163.com>
2019-06-23 22:16:41 +0800

    添加 consul

--- User.API/Startup.cs
+++ User.API/Startup.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Consul;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Server.Features;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -11,6 +14,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using User.API.Data;
+using User.API.Dtos;
 using User.API.Filters;
 
 namespace User.API
@@ -31,21 +35,108 @@ namespace User.API
             {
                 options.UseMySql(Configuration.GetConnectionString("DefaultConnection"));
             });
-            services.AddMvc(option=> {
+
+            services.Configure<Dtos.ServiceDisvoveryOptions>(Configuration.GetSection("ServiceDiscovery"));// json 配置文件映射到实体类
+
+            services.AddMvc(option =>
+            {
                 option.Filters.Add(typeof(GlobalExceptionFilter));
             }).SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IApplicationLifetime application, IOptions<ServiceDisvoveryOptions> serviceOptions, IConsulClient consul)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            // 启动时注册服务
+            application.ApplicationStarted.Register(() =>
+            {
+                RegisterService(app, serviceOptions, consul);
+            });
+
+            // 停止的时候移除服务
+            application.ApplicationStopped.Register(() =>
+            {
+                DeRegisterService(app, serviceOptions, consul);
+            });
+
             app.UseMvc();
             UserContextSeed.SeedAsync(app, loggerFactory).Wait();
+        }
+
+        private void RegisterService(IApplicationBuilder app, IOptions<ServiceDisvoveryOptions> serviceOptions, IConsulClient consul)
+        {
+            var features = app.Properties["server.Features"] as FeatureCollection;
+            var addresses = features.Get<IServerAddressesFeature>()
+                .Addresses
+                .Select(p => new Uri(p));
+
+            foreach (var address in addresses)
+            {
+                var serviceId = $"{serviceOptions.Value.ServiceName}_{address.Host}:{address.Port}";
+
+                var httpCheck = new AgentServiceCheck()
+                {
+                    DeregisterCriticalServiceAfter = TimeSpan.FromMinutes(1),
+                    Interval = TimeSpan.FromSeconds(30),
+                    HTTP = new Uri(address, "HealthCheck").OriginalString
+                };
+
+                var registration = new AgentServiceRegistration()
+                {
+                    Checks = new[] { httpCheck },
+                    Address = address.Host,
+                    ID = serviceId,
+                    Name = serviceOptions.Value.ServiceName,
+                    Port = address.Port
+                };
+
+                consul.Agent.ServiceRegister(registration).GetAwaiter().GetResult();
+
+                //appLife.ApplicationStopping.Register(() =>
+                //{
+                //    consul.Agent.ServiceDeregister(serviceId).GetAwaiter().GetResult();
+                //});
+            }
+        }
+        private void DeRegisterService(IApplicationBuilder app, IOptions<ServiceDisvoveryOptions> serviceOptions, IConsulClient consul)
+        {
+            var features = app.Properties["server.Features"] as FeatureCollection;
+            var addresses = features.Get<IServerAddressesFeature>()
+                .Addresses
+                .Select(p => new Uri(p));
+
+            foreach (var address in addresses)
+            {
+                var serviceId = $"{serviceOptions.Value.ServiceName}_{address.Host}:{address.Port}";
+
+                var httpCheck = new AgentServiceCheck()
+                {
+                    DeregisterCriticalServiceAfter = TimeSpan.FromMinutes(1),
+                    Interval = TimeSpan.FromSeconds(30),
+                    HTTP = new Uri(address, "HealthCheck").OriginalString
+                };
+
+                var registration = new AgentServiceRegistration()
+                {
+                    Checks = new[] { httpCheck },
+                    Address = address.Host,
+                    ID = serviceId,
+                    Name = serviceOptions.Value.ServiceName,
+                    Port = address.Port
+                };
+
+                consul.Agent.ServiceRegister(registration).GetAwaiter().GetResult();
+
+                //appLife.ApplicationStopping.Register(() =>
+                //{
+                //    consul.Agent.ServiceDeregister(serviceId).GetAwaiter().GetResult();
+                //});
+            }
         }
     }
 }
