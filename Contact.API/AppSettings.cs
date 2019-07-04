6ccb12cffe5bdef57414c331eedc6b7e1a90ce08
xiecz <chengzhongxie@163.com>
2019-07-04 13:41:01 +0800

    修改

--- Contact.API/AppSettings.cs
+++ Contact.API/AppSettings.cs
@@ -7,7 +7,13 @@ namespace Contact.API
 {
     public class AppSettings
     {
+        /// <summary>
+        /// MongoDb 链接字符串
+        /// </summary>
         public string MongoContactConnectionString { get; set; }
+        /// <summary>
+        /// MongoDb 数据库
+        /// </summary>
         public string MongoContectDatabase { get; set; }
     }
 }
