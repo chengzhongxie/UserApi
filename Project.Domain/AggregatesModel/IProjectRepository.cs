a5ad2b142337993d23021d553230744008bb90e2
xiecz <chengzhongxie@163.com>
2019-07-12 12:27:05 +0800

    DDD 领域驱动设计添加

---
+++ Project.Domain/AggregatesModel/IProjectRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project.Domain.AggregatesModel
+{
+    /// <summary>
+    /// 领域模型
+    /// </summary>
+    public interface IProjectRepository
+    {
+    }
+}
---
+++ Project.Domain/AggregatesModel/Project.cs
@@ -0,0 +1,12 @@
+﻿using Project.Domain.Seedwork;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project.Domain.AggregatesModel
+{
+    public class Project : Entity, IAggregateRoot
+    {
+
+    }
+}
